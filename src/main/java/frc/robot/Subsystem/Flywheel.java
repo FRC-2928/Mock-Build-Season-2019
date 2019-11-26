6c63e7501361014426a374f5d0bd2d694baf5aac
Jett Kim <jett.kim@gmail.com>
2019-11-25 19:57:13 -0800

    Flywheel lessons. Flywheel subsystem and SetFlywheel velocity Command
    Flywheel lessons. Flywheel subsystem and SetFlywheel velocity Command

--- src/main/java/frc/robot/Subsystem/Flywheel.java
+++ src/main/java/frc/robot/Subsystem/Flywheel.java
@@ -20,15 +20,26 @@ public class Flywheel extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
   public WPI_TalonSRX flyWheelMotor;
+  public int currentposition;
+  public int velocity;
+  public int lastposition;
+
   public Flywheel(){
     flyWheelMotor = new WPI_TalonSRX(RobotMap.FLYWHEEL_MOTOR_ID);
-
+    lastposition = 0;
   }
 public void setFlyWheelSpeed(double power){
   flyWheelMotor.set(ControlMode.PercentOutput, power);
 } 
 public int getflywheelencoder(){
   return flyWheelMotor.getSelectedSensorPosition();
+}
+
+public int getflywheelvelocity(){
+  currentposition =  flyWheelMotor.getSelectedSensorPosition();
+  velocity = currentposition - lastposition; 
+  lastposition = currentposition;
+  return velocity;
 }
  @Override
   public void initDefaultCommand() {
